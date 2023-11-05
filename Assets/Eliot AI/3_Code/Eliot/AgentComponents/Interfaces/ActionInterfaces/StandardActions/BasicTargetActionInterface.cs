4b1ee300810b3f552fe5cafc7edd037140423c61
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-11-05 14:08:42 +0700

    add neccessary package

---
+++ Assets/Eliot AI/3_Code/Eliot/AgentComponents/Interfaces/ActionInterfaces/StandardActions/BasicTargetActionInterface.cs
@@ -0,0 +1,16 @@
+﻿using Eliot.AgentComponents;
+
+public class BasicTargetActionInterface : TargetActionInterface {
+
+	public BasicTargetActionInterface(EliotAgent agent) : base(agent)
+	{
+	}
+
+	/// <summary>
+	/// Reset the target to the default one.
+	/// </summary>
+	[IncludeInBehaviour] public void ResetTarget()
+	{
+		Agent.Target = Agent.GetDefaultTarget();
+	}
+}
