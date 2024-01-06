efb3038d5a83e539ab2db672a26c04f9788c8d83
20120435 <bich1042002@gmail.com>
2024-01-06 16:10:07 +0700

    clickable list item
     clickable list item and display weapon

---
+++ Assets/Game/UI/HomeScreen/Scripts/WeaponsScreen/ScriptableObjectChanger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class ScriptableObjectChanger : MonoBehaviour
+{
+    [SerializeField] private ScriptableObject[] serializableObjects;
+
+	[SerializeField] private WeaponDisplay weaponDisplay;
+
+	[SerializeField] private GameObject weaponList;
+
+	[SerializeField] private GameObject weaponItemPrefab;
+
+
+	private void Awake()
+	{
+		GenerateWeaponList();
+		weaponDisplay.DisplayItem((Weapon)serializableObjects[0]);
+		
+	}
+
+	private void GenerateWeaponList()
+	{
+		foreach (ScriptableObject info in serializableObjects)
+		{
+			// Tạo một WeaponItem từ Prefab
+			GameObject weaponItemObject = Instantiate(weaponItemPrefab, weaponList.transform);
+
+			// Lấy thành phần WeaponItem từ đối tượng mới tạo
+			WeaponIcon weaponIcon = weaponItemObject.GetComponent<WeaponIcon>();
+
+			
+
+			// Kiểm tra xem thành phần có tồn tại không
+			if (weaponIcon != null)
+			{
+				Debug.Log(weaponIcon.ToString());
+				// Thiết lập thông tin cho WeaponItem từ mảng thông tin
+				weaponIcon.DisplayItem((Weapon)info, weaponDisplay);
+			}
+			else
+			{
+				Debug.LogError("WeaponItem component not found on the prefab!");
+			}
+		}
+	}
+
+
+}
